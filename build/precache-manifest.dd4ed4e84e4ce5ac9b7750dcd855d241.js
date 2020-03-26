e8d1cc6d5475e0ce173650ed9bab7e4949d4f3b1
Alex <alex.duncan@rockets.utoledo.edu>
2020-03-26 15:34:08 -0700

    build added

---
+++ build/precache-manifest.dd4ed4e84e4ce5ac9b7750dcd855d241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f86a4ca53ac0eaa0a00ab027a5718d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53df0aed0efbdb29b056",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a036b384803538291d34",
+    "url": "/static/js/2.65490f0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65490f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53df0aed0efbdb29b056",
+    "url": "/static/js/main.d5893720.chunk.js"
+  },
+  {
+    "revision": "30fb9df71181a017c837",
+    "url": "/static/js/runtime-main.c4cbea75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
